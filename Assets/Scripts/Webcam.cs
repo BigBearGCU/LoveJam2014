d3d50e55ebe1d2453fc059a473a85fb7e979ad87
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2014-11-15 11:45:44 +0000

    Initial Commit of content

---
+++ Assets/Scripts/Webcam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Webcam : MonoBehaviour {
+
+    private WebCamTexture webcamTexture;
+
+
+	// Use this for initialization
+	void Start () {
+        webcamTexture = new WebCamTexture();
+        webcamTexture.Play();
+
+        renderer.material.mainTexture = webcamTexture;
+        InvokeRepeating("ProcessImage", 0.0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void ProcessImage()
+    {
+        Color[] data = webcamTexture.GetPixels();
+
+    }
+}
